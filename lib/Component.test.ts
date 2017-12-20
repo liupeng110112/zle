79f1e68ce9a0518f65f1bd8445f9cb6260b051e3
indexmotion <indexmotion@gmail.com>
2017-12-20 10:38:23 +0800

    Remove class Container since we dropped the singleton behavior of components

--- lib/Component.test.ts
+++ lib/Component.test.ts
@@ -8,7 +8,7 @@ describe('Component', () => {
     class Main extends Component {
       definition = this.$root('.main', 'main');
     }
-    const main = context.container.get(Main);
+    const main = new Main(context);
 
     test('can find itself with right name', () => {
       expect(main.$findNodeByName('notexist')).toBeUndefined();
@@ -19,7 +19,7 @@ describe('Component', () => {
       })
     });
 
-    test('walk descendants correctly', () => {
+    test('walk nodes correctly', () => {
       expect(Array.from(main.$walkNodes())).toEqual([{
         $selector: '.main',
         $name: 'main',
@@ -34,7 +34,7 @@ describe('Component', () => {
                            .withDescendant('.nav-bar', 'nav bar')
                            .withDescendant('.content-area');
     }
-    const main = context.container.get(Main);
+    const main = new Main(context);
 
     test('has descendants', () => {
       expect(main.$findNodeByName('main')).toEqual({
@@ -44,13 +44,13 @@ describe('Component', () => {
       })
     });
 
-    test('can find descendant by name', () => {
+    test('can find node by name', () => {
       const node = main.$findNodeByName('nav bar')!;
       expect(node.$selector).toEqual('.nav-bar');
       expect(node.$name).toEqual('nav bar');
     });
 
-    test('walk descendants correctly', () => {
+    test('walk nodes correctly', () => {
       const result = Array.from(main.$walkNodes());
       expect(result[0]).toEqual({
         $selector: '.main',
@@ -81,13 +81,17 @@ describe('Component', () => {
                           .withDescendant('.content-area', 'content area');
     }
 
-    const main = context.container.get(Main);
-    expect(main.$findNodeByName('specific nav bar')).toEqual({
-      $selector: '.nav-bar',
-      $name: 'specific nav bar',
-      $hasDescendants: false
-    })
-    expect(main.$findNodeByName('generic nav bar')).toBeUndefined();
+    const main = new Main(context);
+    test('can find node by name', () => {
+      expect(main.$findNodeByName('specific nav bar')).toEqual({
+        $selector: '.nav-bar',
+        $name: 'specific nav bar',
+        $hasDescendants: false
+      })
+    });
+    test('cannot find nonexistent node', () => {
+      expect(main.$findNodeByName('generic nav bar')).toBeUndefined();
+    });
   });
 
   describe('with many descendants', () => {
@@ -113,54 +117,63 @@ describe('Component', () => {
                           .withDescendant(Footer);
     }
 
-    const main = context.container.get(Main);
-    expect(main.$findNodeByName('magic header')).toEqual({
-      $selector: 'header',
-      $name: 'magic header',
-      $hasDescendants: true
-    });
-    expect(main.$findNodeByName('general header')).toBeUndefined();
-    expect(main.$findNodeByName('hot footer')).toEqual({
-      $selector: 'footer',
-      $name: 'hot footer',
-      $hasDescendants: true
-    });
-    expect(Array.from(main.$walkNodes())).toEqual([
-      {
-        $selector: '.main',
-        $name: undefined,
-        $hasDescendants: true
-      },
-      {
-        $selector: '.nav-bar',
-        $name: undefined,
-        $hasDescendants: false
-      },
-      {
+    const main = new Main(context);
+
+    test('can find node by name', () => {
+      expect(main.$findNodeByName('magic header')).toEqual({
         $selector: 'header',
         $name: 'magic header',
         $hasDescendants: true
-      },
-      {
-        $selector: 'h2',
-        $name: 'post title',
-        $hasDescendants: false
-      },
-      {
+      });
+      expect(main.$findNodeByName('hot footer')).toEqual({
         $selector: 'footer',
         $name: 'hot footer',
         $hasDescendants: true
-      },
-      {
-        $selector: 'span.author',
-        $name: undefined,
-        $hasDescendants: false
-      },
-      {
-        $selector: 'span.publish-date',
-        $name: 'publish date',
-        $hasDescendants: false
-      }
-    ])
+      });
+    });
+
+    test('cannot find nonexistent node', () => {
+      expect(main.$findNodeByName('general header')).toBeUndefined();
+    });
+
+    test('walk nodes correctly', () => {
+      expect(Array.from(main.$walkNodes())).toEqual([
+        {
+          $selector: '.main',
+          $name: undefined,
+          $hasDescendants: true
+        },
+        {
+          $selector: '.nav-bar',
+          $name: undefined,
+          $hasDescendants: false
+        },
+        {
+          $selector: 'header',
+          $name: 'magic header',
+          $hasDescendants: true
+        },
+        {
+          $selector: 'h2',
+          $name: 'post title',
+          $hasDescendants: false
+        },
+        {
+          $selector: 'footer',
+          $name: 'hot footer',
+          $hasDescendants: true
+        },
+        {
+          $selector: 'span.author',
+          $name: undefined,
+          $hasDescendants: false
+        },
+        {
+          $selector: 'span.publish-date',
+          $name: 'publish date',
+          $hasDescendants: false
+        }
+      ]);
+    });
   });
 });
