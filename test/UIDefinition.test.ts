7895ab8b1e137a21fdcc938751815185bc3f35c5
indexmotion <indexmotion@gmail.com>
2017-12-27 20:02:49 +0800

    Re-implement UIDefinition.walkUINodes

--- test/UIDefinition.test.ts
+++ test/UIDefinition.test.ts
@@ -6,74 +6,74 @@ test('UIDefinition', (t) => {
   const definition = Post.$definition;
 
   const post = definition.findUINodeByName('post')!;
-  t.deepEqual(post.path, ['div.post'])
+  t.deepEqual(post.selector, '');
   t.is(post.name, 'post');
   t.truthy(post.hasDescendants);
 
   const header = definition.findUINodeByName('post header')!;
-  t.deepEqual(header.path, ['div.post', 'header']);
+  t.deepEqual(header.selector, 'header');
   t.is(header.name, 'post header');
   t.truthy(header.hasDescendants);
   t.falsy(definition.findUINodeByName('general header'));
 
   const footer = definition.findUINodeByName('post footer')!;
-  t.deepEqual(footer.path, ['div.post', 'footer']);
+  t.deepEqual(footer.selector, 'footer');
   t.is(footer.name, 'post footer');
   t.truthy(footer.hasDescendants);
   
   const commentList = definition.findUINodeByName('comments')!;
-  t.deepEqual(commentList.path, ['div.post', 'footer', 'div.comments ul']);
+  t.deepEqual(commentList.selector, 'footer div.comments ul');
   t.is(commentList.name, 'comments');
   t.falsy(commentList.hasDescendants);
 
   const expections: Array<UINode> = [
     {
-      path: ['div.post'],
+      selector: '',
       name: 'post',
       hasDescendants: true
     },
     {
-      path: ['div.post', 'nav'],
+      selector: 'nav',
       name: undefined,
       hasDescendants: false
     },
     {
-      path: ['div.post', 'header'],
+      selector:  'header',
       name: 'post header',
       hasDescendants: true
     },
     {
-      path: ['div.post', 'header', 'h2'],
+      selector:  'header h2',
       name: 'post title',
       hasDescendants: false
     },
     {
-      path: ['div.post', 'footer'],
+      selector:  'footer',
       name: 'post footer',
       hasDescendants: true
     },
     {
-      path: ['div.post', 'footer', 'span.author'],
+      selector:  'footer span.author',
       name: undefined,
       hasDescendants: false
     },
     {
-      path: ['div.post', 'footer', 'span.publish-date'],
+      selector:  'footer span.publish-date',
       name: 'publish date',
       hasDescendants: false
     },
     {
-      path: ['div.post', 'footer', 'div.comment-composer'],
+      selector:  'footer div.comment-composer',
       name: 'comment composer',
       hasDescendants: true
     },
     {
-      path: ['div.post', 'footer', 'div.comment-composer', 'input'],
+      selector:  'footer div.comment-composer input',
       name: 'comment input field',
       hasDescendants: false
     },
     {
-      path: ['div.post', 'footer', 'div.comments ul'],
+      selector:  'footer div.comments ul',
       name: 'comments',
       hasDescendants: false
     }
@@ -81,7 +81,7 @@ test('UIDefinition', (t) => {
   const nodes = Array.from(definition.walkUINodes());
   t.is(nodes.length, expections.length);
   for (let i = 0; i < nodes.length; i++) {
-    t.deepEqual(nodes[i].path, expections[i].path);
+    t.deepEqual(nodes[i].selector, expections[i].selector);
     t.is(nodes[i].name, expections[i].name);
     t.is(!!nodes[i].hasDescendants, expections[i].hasDescendants);
   }
