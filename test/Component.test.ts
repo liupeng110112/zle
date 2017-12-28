e8a7637351dc816e593f20a72d63a143e53e2a20
indexmotion <indexmotion@gmail.com>
2017-12-28 17:27:51 +0800

    Add $textOf and $htmlOf methods to class Component

---
+++ test/Component.test.ts
@@ -0,0 +1,31 @@
+import { getExecutablePath, getPageUrl } from "./helpers";
+import { test } from "../lib/index";
+import { Post } from "./assets/post.components";
+import { ContextFactory } from "../lib/ContextFactory";
+
+test.beforeEach(async t => {
+  const factory = new ContextFactory();
+  t.context = await factory.$create({
+    executablePath: getExecutablePath()
+  });
+});
+
+test.afterEach(async t => {
+  await t.context.$getBrowser().close();
+});
+
+test("#$textOf", async t => {
+  const page = await t.context.$getPage();
+  await page.goto(getPageUrl("post"));
+  const post = await t.context.$waitFor(Post);
+  const title = await post.$textOf("post title");
+  t.is(title, "Post 1");
+});
+
+test("#$htmlOf", async t => {
+  const page = await t.context.$getPage();
+  await page.goto(getPageUrl("post"));
+  const post = await t.context.$waitFor(Post);
+  const html = await post.$htmlOf("post header");
+  t.is(html.trim(), "<h2>Post 1</h2>");
+});
