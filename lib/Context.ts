9b25465283a54cfd37585b3afe58b3a1a5de94fb
indexmotion <indexmotion@gmail.com>
2018-01-03 19:25:16 +0800

    Implement selectUnique and selectFirst methods to ComponentFactory

--- lib/Context.ts
+++ lib/Context.ts
@@ -24,10 +24,34 @@ export class Context {
     return factory.selectAll(satisfying);
   }
 
-  $$<T extends Component>(
+  $$ = this.selectAll.bind(this) as <T extends Component>(
+    constructor: ComponentConstructor<T>,
+    satisfying?: SatisfyingFunction<T>
+  ) => AsyncIterableIterator<T>;
+
+  selectUnique<T extends Component>(
     constructor: ComponentConstructor<T>,
     satisfying?: SatisfyingFunction<T>
   ) {
-    return this.selectAll(constructor, satisfying);
+    const factory = new ComponentFactory<T>(this, constructor);
+    return factory.selectUnique(satisfying);
   }
+
+  $_ = this.selectUnique.bind(this) as <T extends Component>(
+    constructor: ComponentConstructor<T>,
+    satisfying?: SatisfyingFunction<T>
+  ) => Promise<T | undefined>;
+
+  selectFirst<T extends Component>(
+    constructor: ComponentConstructor<T>,
+    satisfying?: SatisfyingFunction<T>
+  ) {
+    const factory = new ComponentFactory<T>(this, constructor);
+    return factory.selectFirst(satisfying);
+  }
+
+  $ = this.selectFirst.bind(this) as <T extends Component>(
+    constructor: ComponentConstructor<T>,
+    satisfying?: SatisfyingFunction<T>
+  ) => Promise<T | undefined>;
 }
--- test/Context.test.ts
+++ test/Context.test.ts
@@ -2,6 +2,7 @@ import { getExecutablePath, getPageUrl } from "./helpers";
 import { test } from "../lib/index";
 import { Post } from "./assets/post.components";
 import { ContextFactory } from "../lib/ContextFactory";
+import { TodoApp } from "./assets/todo.components";
 
 test.beforeEach(async t => {
   const factory = new ContextFactory();
@@ -45,4 +46,51 @@ test("#$$ with satisfying function", async t => {
   }
   t.is(posts.length, 1);
   t.is(await posts[0].getTitle(), "Post 3");
+});
+
+test("#$_ with not unique component", async t => {
+  const page = t.context.page;
+  await page.goto(getPageUrl("post"));
+  t.is(await t.context.$_(Post), undefined);
+});
+
+test("#$_ with unique component", async t => {
+  const page = t.context.page;
+  await page.goto(getPageUrl("todo"));
+  const todoApp = await t.context.$_(TodoApp);
+  t.truthy(todoApp instanceof TodoApp);
+});
+
+test("#$_ with satisfying function", async t => {
+  const page = t.context.page;
+  await page.goto(getPageUrl("post"));
+  const post = await t.context.$_(
+    Post,
+    async post => (await post.getTitle()) === "Post 3"
+  );
+  t.truthy(post instanceof Post);
+  t.is(await post!.getTitle(), "Post 3");
+});
+
+test("#$", async t => {
+  const page = t.context.page;
+  await page.goto(getPageUrl("post"));
+  const post = await t.context.$(Post);
+  t.is(await post!.getTitle(), "Post 1");
+});
+
+test("#$ with not existed component", async t => {
+  const page = t.context.page;
+  await page.goto(getPageUrl("post"));
+  t.is(await t.context.$(TodoApp), undefined);
+});
+
+test("#$ with satisfying component", async t => {
+  const page = t.context.page;
+  await page.goto(getPageUrl("post"));
+  const post = await t.context.$(
+    Post,
+    async post => (await post.getTitle()) === "Post 3"
+  );
+  t.is(await post!.getTitle(), "Post 3");
 });
