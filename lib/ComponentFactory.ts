74012ca8f34dfc5756a14041b609d751b2d4fb88
indexmotion <indexmotion@gmail.com>
2018-01-03 16:49:31 +0800

    ComponentFactory.constructor is bound to context and constructor

--- lib/ComponentFactory.ts
+++ lib/ComponentFactory.ts
@@ -10,57 +10,50 @@ export type SatisfyingFunction<T> = (component: T) => Promise<boolean>;
 
 export class ComponentFactory<T extends Component>
   implements IAsyncFactory<T>, IDisplayObjectFactory<T> {
-  protected satisfyingStrategy = new ComponentSatisfyingStrategy(this.context);
+  constructor(
+    protected context: Context,
+    protected _constructor: ComponentConstructor<T>
+  ) {}
 
-  constructor(protected context: Context) {}
-
-  async create(
-    constructor: ComponentConstructor<T>,
-    context: Context,
-    handle: ElementHandle
-  ) {
-    return new constructor(context, handle);
+  async create(handle: ElementHandle) {
+    return new this._constructor(this.context, handle);
   }
 
-  async waitFor(
-    constructor: ComponentConstructor<T>,
-    timeout?: number,
-    scope?: Component
-  ) {
+  async waitFor(timeout?: number, scope?: Component) {
     const selector = scope
-      ? [await scope.$getSelector(), constructor.$definition.selector].join(" ")
-      : constructor.$definition.selector;
-    const strategies = await this.satisfyingStrategy.getStrategy(
-      constructor,
-      timeout,
-      selector
+      ? [
+          await scope.$getSelector(),
+          this._constructor.$definition.selector
+        ].join(" ")
+      : this._constructor.$definition.selector;
+    const strategy = new ComponentSatisfyingStrategy(this.context);
+    await Promise.all(
+      await strategy.getStrategy(this._constructor, timeout, selector)
     );
-    await Promise.all(strategies);
     const page = this.context.page;
     const handle = await page.$(selector);
     if (handle) {
-      const component = await this.create(constructor, this.context, handle);
+      const component = await this.create(handle);
       return component;
     } else {
       throw new Error(
         `Cannot locate component "${
-          constructor.name
+          this._constructor.name
         }" by selector "${selector}"`
       );
     }
   }
 
-  async *selectAll(
-    constructor: ComponentConstructor<T>,
-    satisfying?: SatisfyingFunction<T>,
-    scope?: Component
-  ) {
+  async *selectAll(satisfying?: SatisfyingFunction<T>, scope?: Component) {
     const page = this.context.page;
     const selector = scope
-      ? [await scope.$getSelector(), constructor.$definition.selector].join(" ")
-      : constructor.$definition.selector;
+      ? [
+          await scope.$getSelector(),
+          this._constructor.$definition.selector
+        ].join(" ")
+      : this._constructor.$definition.selector;
     for (let handle of await page.$$(selector)) {
-      const component = await this.create(constructor, this.context, handle);
+      const component = await this.create(handle);
       if (!satisfying || (await satisfying(component))) {
         yield component;
       } else {
