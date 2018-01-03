af1441bc75015f4ca758dcac0e35f772f64141de
indexmotion <indexmotion@gmail.com>
2018-01-03 15:41:38 +0800

    Refactor the implementation of ComponentFactory.waitFor

--- lib/ComponentFactory.ts
+++ lib/ComponentFactory.ts
@@ -20,49 +20,31 @@ export class ComponentFactory<T extends Component>
     return new constructor(context, handle);
   }
 
-  waitFor(
+  async waitFor(
     constructor: ComponentConstructor<T>,
     timeout?: number,
     scope?: Component
   ) {
-    return new Promise<T>(async (resolve, reject) => {
-      try {
-        const selector = scope
-          ? [await scope.$getSelector(), constructor.$definition.selector].join(
-              " "
-            )
-          : constructor.$definition.selector;
-        const strategies = await this.satisfyingStrategy.getStrategy(
-          constructor,
-          timeout,
-          selector
-        );
-        await Promise.all(strategies);
-        const page = this.context.getPage();
-        const handle = await page.$(selector);
-        if (handle) {
-          try {
-            const component = await this.create(
-              constructor,
-              this.context,
-              handle
-            );
-            resolve(component);
-          } catch (err) {
-            reject(err);
-          }
-        } else {
-          reject(
-            new Error(
-              `Cannot locate component "${
-                constructor.name
-              }" by selector "${selector}"`
-            )
-          );
-        }
-      } catch (err) {
-        reject(err);
-      }
-    });
+    const selector = scope
+      ? [await scope.$getSelector(), constructor.$definition.selector].join(" ")
+      : constructor.$definition.selector;
+    const strategies = await this.satisfyingStrategy.getStrategy(
+      constructor,
+      timeout,
+      selector
+    );
+    await Promise.all(strategies);
+    const page = this.context.getPage();
+    const handle = await page.$(selector);
+    if (handle) {
+      const component = await this.create(constructor, this.context, handle);
+      return component;
+    } else {
+      throw new Error(
+        `Cannot locate component "${
+          constructor.name
+        }" by selector "${selector}"`
+      );
+    }
   }
 }
