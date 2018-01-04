96c5a4e37338bccf0db2dd324d232113f2c7684b
indexmotion <indexmotion@gmail.com>
2018-01-04 15:15:19 +0800

    Refine type definition

--- test/assets/rect.components.ts
+++ test/assets/rect.components.ts
@@ -2,15 +2,11 @@ import { Component } from '../../lib/Component';
 import { UIDefinition } from '../../lib/UIDefinition';
 
 export class Rect extends Component {
-  static $definition = UIDefinition.root(
-    "div.rect",
-    "rect",
-    (el: HTMLElement) => {
-      return new Promise((resolve, _) => {
-        el.addEventListener("transitionend", () => {
-          resolve();
-        });
+  static $definition = UIDefinition.root("div.rect", "rect", el => {
+    return new Promise((resolve, _) => {
+      el.addEventListener("transitionend", () => {
+        resolve();
       });
-    }
-  );
+    });
+  });
 }
