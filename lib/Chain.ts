1d1da3750239572a4ea9af83b66b25906e82c51a
indexmotion <indexmotion@gmail.com>
2018-01-22 20:03:07 +0800

    Exit process early if developer forgot to invoke "$done" at the end of method chaining

--- lib/Chain.ts
+++ lib/Chain.ts
@@ -4,12 +4,24 @@ export type Chainable<T> = {
 
 export function chain<T>(bootstrap: () => Promise<T>) {
   const instructions = new Array<[string, any[]]>();
+  const semaphore = {
+    isDone: false
+  };
+  setTimeout(() => {
+    if (!semaphore.isDone) {
+      console.error(
+        `[Did you forget to invoke "$done" at the end of method chaining?]`
+      );
+      process.exit(1);
+    }
+  }, process.env.ZLE_WAIT_FOR_CHAIN_DONE_TIMEOUT || 10000);
   const proxy = new Proxy(
     {},
     {
       get: (_, name: string) => {
         return (...args: any[]) => {
           if (name === "$done") {
+            semaphore.isDone = true;
             return (async () => {
               try {
                 let intermediate: any = await bootstrap();
