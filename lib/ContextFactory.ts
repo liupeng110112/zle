38d8bb3c5f358ab10683d8ca1a78c5e38edbab71
indexmotion <indexmotion@gmail.com>
2018-04-18 16:19:46 +0800

    Polyfill at ContextFactory's initialization

--- lib/ContextFactory.ts
+++ lib/ContextFactory.ts
@@ -2,15 +2,16 @@ import { Browser } from "puppeteer";
 import { Context } from "./Context";
 
 export class ContextFactory {
-  constructor(protected browser: Browser) {}
-  async create() {
+  constructor(protected browser: Browser) {
     // Polyfill for "for await"
     // Reference: https://github.com/Microsoft/TypeScript/issues/14151
     // Reference: https://stackoverflow.com/questions/43694281/ts2318-cannot-find-global-type-asynciterableiterator-async-generator
     (<any>Symbol)["asyncIterator"] =
       Symbol.asyncIterator || Symbol.for("asyncIterator");
     // End of polyfill
+  }
 
+  async create() {
     const page = await this.browser.newPage();
     return new Context(page);
   }
